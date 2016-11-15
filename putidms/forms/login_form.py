9b78aece2f934aea61dbd353b9cb92cd00adb213
simonli <simonli@live.com>
2016-11-15 09:04:55 +0800

    Add Validators

--- putidms/forms/login_form.py
+++ putidms/forms/login_form.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 from wtforms import Form, StringField, PasswordField, validators
 
-
 class LoginForm(Form):
-    username = StringField(u'用户名', validators=[validators.input_required(u'用户名不能为空')])
+    username = StringField(u'用户名', validators=[validators.input_required(u'用户名不能为空'),validators.data_required()])
     password = PasswordField(u'密码', validators=[validators.input_required(u'密码不能为空')])

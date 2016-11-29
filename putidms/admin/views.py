438f9a6ea5df6a7d260418a9c22c68cae058e1ae
simonli <simonli@live.com>
2016-11-29 15:03:18 +0800

    change models from packages to modules

--- putidms/admin/views.py
+++ putidms/admin/views.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from flask import Blueprint, request, render_template, redirect, url_for, flash
-from putidms.models.user import User
+from putidms.models import User
 from flask_login import login_user, logout_user, login_required
 from putidms import db
 
